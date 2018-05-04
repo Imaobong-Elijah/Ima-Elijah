a73d9a40093705392a053d176540bb3ede4a0a44
Imaobong-Elijah <imaeliobot@gmail.com>
2018-05-04 01:08:11 +0100

    timedate page upload

---
+++ timedate.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <? php date_default_timezone_set(Africa/Lagos);?> -->
+</head>
+    <style>
+    
+       
+body{ 
+    background-image: url("https://res.cloudinary.com/dbvitxz4y/image/upload/v1525365863/bg.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed; 
+    /* background-size: 100% ; */
+}
+h1{
+    font-family: Arial;
+    font-size: 50px;
+    color: #DDDDDD;
+    text-align: center;
+    /* background-position: center top; */
+    
+}
+.time{
+        font-family: Arial;
+        font-size: 40px 24px;
+        color: #DDDDDD;
+        padding-left: 80%; 
+        padding-top: 30%;
+        /* padding-right: 300px; */
+}
+    </style>
+</head> 
+<body>
+    <h1>
+        HOTELS.NG INTERNSHIP 4
+    </h1>
+    <div class="time"><br>
+<?php
+echo date ("d:M.Y"), '<br>';
+echo date ("h:i:sA"), '<br>';
+?>
+    </div>
+</body>
+</html>
+        
